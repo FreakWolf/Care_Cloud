939834c6bc756ea67627e5f5306894a3075493b5
shayan-shaikh <65915426+shayan-shaikh@users.noreply.github.com>
2023-08-31 17:25:17 +0530

    Home page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image } from 'react-native';
+import * as Font from 'expo-font';
+import { useEffect } from 'react';
+import DNALogo from './assets/images/DNALogo.png'
 export default function App() {
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        'PaytoneOne-Regular': require('./assets/fonts/PaytoneOne-Regular.ttf'),
+      });
+    }
+    loadFonts();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>It's just the Setup for the project... :)</Text>
+      <Text style={styles.title}>Care Cloud</Text>
+      <Image source={DNALogo} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +29,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FFFFFF'
+  },
+  title: {
+    fontFamily: 'PaytoneOne-Regular',
+    fontSize: 48,
+    color: '#7AE284'
   }
 });
